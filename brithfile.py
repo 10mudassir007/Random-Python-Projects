1427ebe3afd4ca0b47fe429e33b75ba551a52997
Mudassir Junejo <muddassir032@gmail.com>
2025-11-06 22:33:07 +0500

    structured

--- brithfile.py
+++
@@ -1,7 +0,0 @@
-def calc_age(birth_year,curr_year):
-	return curr_year-birth_year
-	
-x = int(input("Enter current year"))
-y = int(input("Enter birth year"))
-
-print(f"you are {calc_age(y,x)} years old")
--- edges_in_images.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image,ImageFilter
-
-before = Image.open('c:/Users/mudda/OneDrive/Desktop/2.jpg')
-after = before.filter(ImageFilter.FIND_EDGES)
-after.save('img.jpg')
-
